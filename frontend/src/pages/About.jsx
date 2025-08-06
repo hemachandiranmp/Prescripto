6f3c2d89e207c35a4f6686690f04828202536f64
Hemachandiran M P <hemachandiranmp2005@gmail.com>
2025-08-07 00:03:04 +0530

    frontend file

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-center text-2xl pt-10 text-gray-600'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]'src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Welcome to prescripto, your trusted partner in managing your healthcare.</p>
+          <p>Prescripto is commited to excellence in healthcare technology</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Our vision at prescripto is to create a seamless healthcare exprience for every patient.</p>
+        </div>
+      </div>
+
+      <div className='text-xl my-4'>
+        <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className=' flex flex-col md:flex-row mb-20'>
+        <div className=' border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+            <b>Efficiency:</b>
+            <p>Streaming appoinment scheduling that fits into your busy lifestyle.</p>
+        </div>
+            
+        <div className=' border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+            <b>Convenience:</b>
+            <p>Access to anetwork of trusted healthcare professionals in your area.</p>
+        </div>
+
+        <div className=' border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+            <b>Personalization</b>
+            <p>Tailored recommendationa and remainders to help you stay on top of your health.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
